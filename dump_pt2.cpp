6bb01f6a6baf10ebdb3196eb41f830be7d4c8cfd
Ben Gamari <bgamari.foss@gmail.com>
2009-12-13 23:45:52 -0500

    Output special records as well

--- dump_pt2.cpp
+++ dump_pt2.cpp
@@ -8,10 +8,14 @@ int main(int argc, char** argv) {
 
 	for (int i=0; i < pt2.tttr_hdr.n_records; i++) {
 		pt2_record rec = pt2.read_record();
-		if (rec.special)
-			continue;
-
-		printf("%llu			%d\n", rec.time, rec.channel);
+		if (rec.special) {
+			printf("# ");
+			if (rec.markers == 0)
+				printf("overflow\n");
+			else
+				printf("marker %d\n", rec.markers);
+		} else
+			printf("%7llu\t%d\n", rec.time, rec.channel);
 	}
 	return 0;
 }
