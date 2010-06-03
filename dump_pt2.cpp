6e9f68467626a4648e32469ab585f4b60c9bab83
Ben Gamari <bgamari.foss@gmail.com>
2010-06-02 20:50:07 -0400

    Fix signedness

--- dump_pt2.cpp
+++ dump_pt2.cpp
@@ -27,7 +27,7 @@ int main(int argc, char** argv) {
 	std::ifstream is(argv[1]);
 	pt2_file pt2(is);
 
-	for (int i=0; i < pt2.tttr_hdr.n_records; i++) {
+	for (unsigned int i=0; i < pt2.tttr_hdr.n_records; i++) {
 		pt2_record rec = pt2.read_record();
 		if (rec.special) {
 			printf("# ");
