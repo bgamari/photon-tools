24a1465704b7622acb52582785d42542f097d22c
Ben Gamari <bgamari.foss@gmail.com>
2010-07-13 12:48:56 -0400

    extract_pt2_timestamps: Add ability to select dumped channel

--- extract_pt2_timestamps.cpp
+++ extract_pt2_timestamps.cpp
@@ -18,21 +18,22 @@
  * Author: Ben Gamari <bgamari@physics.umass.edu>
  */
 
-
+#include <boost/format.hpp>
 #include <iostream>
 #include <fstream>
 #include "pt2.h"
 
 const double resolution = 1e-7;
 
-void dump(std::istream& is, std::ostream& os) {
+// channel=0xf dumps all channels
+void dump(std::istream& is, std::ostream& os, uint8_t channel=0xf) {
         pt2_file pt2(is);
 	double scale = PT2_TIME_UNIT / resolution;
 	unsigned int n_rec = pt2.tttr_hdr.n_records;
 
 	for (int i=0; i < n_rec; i++) {
 		pt2_record rec = pt2.read_record();
-		if (!rec.special) {
+		if (!rec.special && (channel == 0xf || channel == rec.channel)) {
 			uint64_t time = (uint64_t) rec.time * scale;
 			int count = 1;
 			std::cout.write((char*) &time, sizeof(uint64_t));
@@ -44,7 +45,18 @@ int main(int argc, char** argv) {
         if (argc > 1) {
                 std::string name = argv[1];
                 std::ifstream is(name);
-                std::ofstream os(name + ".times");
+                std::string out_name;
+                uint8_t chan;
+
+                if (argc > 2) {
+                        chan = atoi(argv[2]);
+                        out_name = (boost::format("%s.times") % name).str();
+                } else {
+                        chan = 0xf;
+                        out_name = (boost::format("%s-ch%d.times") % name % chan).str();
+                }
+
+                std::ofstream os(out_name);
                 dump(is, os);
         } else
                 dump(std::cin, std::cout);
