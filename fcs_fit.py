0bdc314deaec4d02f3baf452a753db3454b18538
Ben Gamari <bgamari.foss@gmail.com>
2010-07-21 03:49:01 -0400

    Remove single data-set fitting code
    The global fitting code supercedes this

--- fcs_fit.py
+++ fcs_fit.py
@@ -40,10 +40,6 @@ def load_favia(file):
         var = data[:,4]
         return times, counts, var
 
-def fitfunc(p, y, x, var):
-	err = y - model(p, x)
-	return err / var
-
 def model(p, x):
 	tau_d = p[0]
 	a = p[1]
@@ -56,65 +52,7 @@ def model(p, x):
 	
 	return (1.0 / n) * b * sqrt(c)
 
-
-def fit(times, counts, var):
-        # Parameters: [ tau_d, a, N ]
-        amp = mean(counts[:5])
-        p0 = [ 200e-6, 10, 1/(amp-1) ]
-
-        params, cov_x, infodict, mesg, ier = leastsq(fitfunc, p0, args=(counts, times, var), full_output=True)
-        return params
-
-def fit_single(data):
-        times, counts, var = load_favia(data)
-
-        # Subtract out offset
-        counts -= 1.0
-
-        # Eliminate early data
-        low_time = 1e-6
-        var = var[times > low_time]
-        counts = counts[times > low_time]
-        times = times[times > low_time]
-
-        # Run fit
-        params = fit(times, counts, var)
-        resid = counts - model(params, times)
-
-        # Plot results
-        from matplotlib import pyplot as pl
-        from mpl_toolkits.axes_grid1 import make_axes_locatable
-        pl.title(data.name)
-        ax = pl.subplot(111)
-
-        x = linspace(min(times), max(times), 1e6)
-        ax.semilogx(x, model(params, x), label='Fit')
-        ax.semilogx(times, counts, label=data.name, linestyle='None', marker='+')
-        ax.set_xlabel(r'$\tau$')
-        ax.set_ylabel(r'$G$')
-        ax.legend()
-
-        text = [
-                r'$\tau_d = \mathrm{%1.3e}$' % params[0],
-                r'$a = \mathrm{%1.3e}$' % params[1],
-                r'$N = \mathrm{%1.3e}$' % params[2],
-        ]
-        pl.figtext(0.70, 0.40, "\n".join(text))
-
-        divider = make_axes_locatable(ax)
-        ax_resid = divider.append_axes("top", 1.4, pad=0.0, sharex=ax)
-        ax_resid.axhline(0, color='black')
-        ax_resid.errorbar(times, resid, yerr=var, linestyle='None', marker='x')
-        pl.setp(ax_resid.get_xticklabels(), visible=False)
-        ax_resid.set_ylabel(r'Fit Residuals')
-       
-        ax.autoscale_view(tight=True, scalex=True)
-        pl.draw()
-        pl.show()
-
-        return params
-
-def global_fitfunc(p, data):
+def fitfunc(p, data):
         res = []
         for d, n in zip(data, p[2:]):
                 p1 = list(p[0:2]) + [n]
@@ -122,10 +60,10 @@ def global_fitfunc(p, data):
                 res.extend(err / d.var)
 	return numpy.array(res)
 
-def global_fit(data):
+def fit(data):
         est_n = lambda d: 1/(mean(d.counts[:5]) - 1)
         p0 = [ 200e-6, 10 ] + map(est_n, data)
-        params, cov_x, infodict, mesg, ier = leastsq(global_fitfunc, p0, args=(data), full_output=True)
+        params, cov_x, infodict, mesg, ier = leastsq(fitfunc, p0, args=(data), full_output=True)
         return params
 
 data = []
@@ -144,7 +82,7 @@ for f in sys.argv[1:]:
         data.append(DataSet(f, times, counts, var))
 
 # Run fit
-params = global_fit(data)
+params = fit(data)
 
 # Plot results
 from matplotlib import pyplot as pl
