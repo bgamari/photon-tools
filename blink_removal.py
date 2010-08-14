091c8b63d536c8dbf7e7b4546ec6ffbf090499ea
Ben Gamari <bgamari.foss@gmail.com>
2010-08-14 16:34:20 -0400

    Work on blink removal

--- blink_removal.py
+++ blink_removal.py
@@ -10,12 +10,14 @@ This follows the work of Taylor, et al.
 """
 
 import logging
-from math import pi, exp
+from math import pi, exp, log
 import random
 import numpy as np
 from numpy import mean, array
+from numpy.lib.recfunctions import stack_arrays 
 
 logging.basicConfig(level=logging.DEBUG)
+plot_len = 4000
 
 dtype = np.dtype({'names': ['A','D'], 'formats': [np.uint32,np.uint32]})
 class fret_trajectory:
@@ -68,10 +70,13 @@ class fret_trajectory:
                 return self.prior_NB_prob(Na) * PNB / y
 
         def find_D_blinks(self):
-                _max = max(self.fret_bins.D)
-                _min = min(self.fret_bins.D)
-                center = (_max - _min)/2 + _min
-                blinks = self.fret_bins.D < center
+                Dmax = max(self.fret_bins.D)
+                Dmin = min(self.fret_bins.D)
+                Dcenter = (Dmax - Dmin)/2 + Dmin
+                Amax = max(self.fret_bins.A)
+                Amin = min(self.fret_bins.A)
+                Acenter = (Amax - Amin)/2 + Amin
+                blinks = (self.fret_bins.D < Dcenter) & (self.fret_bins.A < Acenter)
                 return blinks
 
         def find_A_blinks(self, bayes_thresh=2.0):
@@ -84,22 +89,23 @@ class fret_trajectory:
                         NB = self.post_NB_prob(PB, PNB, self.fret_bins.A)
                         bayes = B / NB
                         if True:
-                                rng = 100000
                                 from matplotlib import pyplot as pl
                                 from mpl_toolkits.axes_grid1 import make_axes_locatable
                                 pl.clf()
                                 ax = pl.subplot(111)
-                                ax.plot(self.fret_bins.A[:rng], label='Acceptor')
-                                ax.plot(self.fret_bins.D[:rng], label='Donor')
+                                ax.plot(self.fret_bins.A[:plot_len], label='Acceptor')
+                                ax.plot(self.fret_bins.D[:plot_len], label='Donor')
                                 ax.legend()
                                 divider = make_axes_locatable(ax)
                                 cax = divider.append_axes('bottom', size='100%', pad=0.2)
-                                cax.plot(B[:rng], label='P(B|Na)')
-                                cax.plot(NB[:rng], label='P(NB|Na)')
+                                cax.plot(B[:plot_len], label='P(B|Na)')
+                                cax.plot(NB[:plot_len], label='P(NB|Na)')
                                 cax.legend()
                                 cax = divider.append_axes('bottom', size='100%', pad=0.4)
-                                cax.plot(bayes[:rng], label='Bayes factor')
-                                cax.set_ylim(0, bayes_thresh*4)
+                                x = range(len(bayes[:plot_len]))
+                                cax.fill_between(x, bayes[:plot_len], color='b')
+                                cax.axhline(bayes_thresh, color='r')
+                                #cax.set_ylim(0, 8*bayes_thresh)
                                 pl.savefig('iter%d.png' % i)
 
                         blinks = bayes > bayes_thresh
@@ -123,13 +129,11 @@ class fret_trajectory:
 ###
 ### Test Code
 ###
-def test_data(transitions=1e2, bg_flux=10, flux=(220, 10), fret_eff=0.40, ct_prob=0.005):
+def test_data(transitions=1e3, bg_flux=10, flux=(220, 10), fret_eff1=0.40, fret_eff2=0.7, ct_prob=0.005):
         """ Produce fake FRET data. """
         fret_length = int(round(transitions*0.75))
         ct_length = int(round(transitions*0.25))
-        bg_length = transitions * 5e2
-        d_blink_prob = 0.2
-        a_blink_prob = 0.2
+        bg_length = 10000
 
         def noisify_bins(bins):
                 # Cross-talk
@@ -142,43 +146,54 @@ def test_data(transitions=1e2, bg_flux=10, flux=(220, 10), fret_eff=0.40, ct_pro
                 np.place(bins.A, bins.A<0, 0)
                 np.place(bins.D, bins.D<0, 0)
 
-        # FRET region
-        fret_a_bins, fret_d_bins = [], []
-        for i in range(fret_length):
-                if random.random() < d_blink_prob:
-                        # Donor blink
-                        length = random.randint(0, 5*100)
-                        fret_d_bins.append(np.zeros(length))
-                        fret_a_bins.append(np.zeros(length))
-                elif random.random() < a_blink_prob:
-                        # Acceptor blink
-                        length = random.randint(0, 5*100)
-                        fret_d_bins.append(np.random.normal(flux[0], flux[1], (length)).round())
-                        fret_a_bins.append(np.zeros(length))
-                else:
-                        # No blinking
-                        length = int(random.normalvariate(1e4, 1e3))
-                        fret_d_bins.append(np.random.normal((1-fret_eff)*flux[0], flux[1], (length)).round())
-                        fret_a_bins.append(np.random.normal(fret_eff*flux[0], flux[1], (length)).round())
+        def kinetic_mc(states, steps):
+                state_traj = []
+                total_rate = sum(rate for rate,f in states.values())
+                for i in range(steps):
+                        s = random.random()*total_rate
+                        for state, (rate,f) in states.items():
+                                if rate > s:
+                                        state_traj.append(state)
+                                        dt = int(round(-log(random.random()) / total_rate))
+                                        #print state, dt
+                                        f(dt)
+                                        break
+                                else:
+                                        s -= rate
 
-        fret_bins = np.rec.fromarrays([np.hstack(fret_a_bins), np.hstack(fret_d_bins)], names='A,D')
+
+                return state_traj
+
+
+        bins = []
+        add_bins = lambda a,d: bins.append(np.rec.fromarrays((a,d), names='A,D'))
+
+        # FRET region
+        #       State   : (rate, event_func)
+        states = {
+                'Ablink': (2e-3, lambda l: add_bins(np.zeros(l),
+                                                    np.random.normal(flux[0], flux[1], l).round())),
+                'Dblink': (2e-3, lambda l: add_bins(np.zeros(l),
+                                                    np.zeros(l))),
+                'fret1' : (8e-3, lambda l: add_bins(np.random.normal((1-fret_eff1)*flux[0], flux[1], l).round(),
+                                                    np.random.normal(fret_eff1*flux[0], flux[1], l).round())),
+                'fret2' : (8e-3, lambda l: add_bins(np.random.normal((1-fret_eff2)*flux[0], flux[1], l).round(),
+                                                    np.random.normal(fret_eff2*flux[0], flux[1], l).round())),
+        }
+        state_traj = kinetic_mc(states, fret_length)
+        fret_bins = stack_arrays(bins, asrecarray=True)
         noisify_bins(fret_bins)
 
         # Crosstalk region (acceptor died)
-        ct_a_bins, ct_d_bins = [], []
-        for i in range(ct_length):
-                if random.random() < d_blink_prob:
-                        # Donor blink
-                        length = random.randint(0, 5*100)
-                        ct_d_bins.append(np.zeros(length))
-                        ct_a_bins.append(np.zeros(length))
-                else:
-                        # No blinking
-                        length = round(random.normalvariate(1e4, 1e3))
-                        ct_d_bins.append(np.random.normal(flux[0], flux[1], (length)))
-                        ct_a_bins.append(np.zeros(length))
-
-        ct_bins = np.rec.fromarrays([np.hstack(ct_a_bins), np.hstack(ct_d_bins)], names='A,D')
+        bins = []
+        states = {
+                'Dblink': (2e-3, lambda l: add_bins(np.zeros(l),
+                                                    np.zeros(l))),
+                'obs'   : (8e-3, lambda l: add_bins(np.zeros(l), 
+                                                    np.random.normal(flux[0], flux[1], l).round())),
+        }
+        state_traj = kinetic_mc(states, fret_length)
+        ct_bins = stack_arrays(bins, asrecarray=True)
         noisify_bins(ct_bins)
 
         # Background region
@@ -191,27 +206,38 @@ def test_data(transitions=1e2, bg_flux=10, flux=(220, 10), fret_eff=0.40, ct_pro
         return fret_bins, ct_bins, bg_bins
 
 if __name__ == '__main__':
-        bayes_thresh = 1e6
+        bayes_thresh = 1e2
 
         from matplotlib import pyplot as pl
-        fret,ct,bg = test_data(transitions=1e2)
-        bins = np.hstack((fret,ct,bg))
+        fret,ct,bg = test_data(transitions=1e3)
+        bins = stack_arrays((fret,ct,bg))
 
         pl.clf()
-        pl.plot(bins['A'], label='Acceptor')
-        pl.plot(bins['D'], label='Donor')
+        pl.plot(fret.D[:plot_len], label='Donor')
+        pl.plot(fret.A[:plot_len], label='Acceptor')
         pl.legend()
         pl.savefig('initial.png')
 
         traj = fret_trajectory.from_bins(fret, ct, bg)
-        blinks,_,_ = traj.find_A_blinks(bayes_thresh)
-        deblinked = fret[~blinks]
+        blinksA,_,_ = traj.find_A_blinks(bayes_thresh)
+        blinksD = traj.find_D_blinks()
+        deblinked = fret[~blinksA & ~blinksD]
+        #deblinked = fret[~blinksA]
 
         pl.clf()
-        pl.plot(fret.D, label='Donor')
-        pl.plot(fret.A, label='Acceptor')
+        pl.plot(fret.D[:plot_len], label='Donor')
+        pl.plot(fret.A[:plot_len], label='Acceptor')
         ymin, ymax = pl.ylim()
-        pl.vlines(np.nonzero(blinks)[0], ymin, ymax, alpha=0.3, color='r', label='Blinks')
+        b = np.nonzero(blinksD)[0]
+        pl.vlines(b[b<plot_len], ymin, ymax, alpha=0.1, color='y', label='Donor blinks')
+        b = np.nonzero(blinksA)[0]
+        pl.vlines(b[b<plot_len], ymin, ymax, alpha=0.1, color='r', label='Acceptor blinks')
         pl.legend()
         pl.savefig('fret.png')
         
+        pl.clf()
+        pl.plot(deblinked['D'][:plot_len], label='Donor')
+        pl.plot(deblinked['A'][:plot_len], label='Acceptor')
+        pl.legend()
+        pl.savefig('deblinked.png')
+
