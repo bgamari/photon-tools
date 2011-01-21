2db448c1dd62e878f3b544fea15610a1795e6fe7
Ben Gamari <bgamari.foss@gmail.com>
2011-01-21 16:28:04 -0500

    bin_photons test should check number of bins as well

--- test_bin_photons.py
+++ test_bin_photons.py
@@ -12,11 +12,8 @@ def bin_data(data, bin_len):
 
         fo = tempfile.TemporaryFile('rw+b')
         subprocess.check_call(['./bin_photons', str(bin_len)], stdin=fi, stdout=fo)
-        fo.seek(0)
-        open('asdf', 'w').write(fo.read())
-        fo.seek(0)
         dt = np.dtype([('time', 'u8'), ('counts','u2')])
-        return np.fromfile('asdf', dtype=dt)
+        return np.fromfile(fo, dtype=dt)
 
 test_n = 1
 def result(result, description):
@@ -29,10 +26,10 @@ print '1..3'
 
 di = np.arange(1001, dtype='u8')
 do = bin_data(di, 10)
-result(all(do['counts'][0:99] == 10), 'basic binning')
+result(len(do) == 100 and all(do['counts'][0:99] == 10), 'basic binning')
 
 di = np.array([0, 100, 110], dtype='u8')
 do = bin_data(di, 10)
-res = do['counts'][0] == 1 and all(do['counts'][1:9] == 0) and do['counts'][10] == 1
+res = len(do) == 11 and do['counts'][0] == 1 and all(do['counts'][1:9] == 0) and do['counts'][10] == 1
 result(res, 'bin_photons zero bins test')
 
