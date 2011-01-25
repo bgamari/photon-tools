7a24324ea84ec78251893a73477fe62298481edd
Ben Gamari <bgamari.foss@gmail.com>
2011-01-25 08:41:43 -0500

    Add simple-minded parser for order of magnitude suffixes

---
+++ suffix_parse.py
@@ -0,0 +1,17 @@
+suffixes = {
+        'T': 1e+12,
+        'G': 1e+9,
+        'M': 1e+6,
+        'k': 1e+3,
+        'c': 1e-2,
+        'm': 1e-3,
+        'u': 1e-6,
+        'n': 1e-9,
+        'p': 1e-12
+}
+
+def parse(v):
+        if v[-1] in suffixes:
+                return float(v[:-1]) * suffixes[v[-1]]
+        return float(v)
+
