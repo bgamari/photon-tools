a67966dd3e833f6cabb2939055759bd0d97a2f69
Ben Gamari <bgamari.foss@gmail.com>
2011-02-20 21:24:14 -0500

    change_point: Initial commit

--- change_point.py
+++ change_point.py
@@ -9,20 +9,7 @@ This follows the work of Yang and Watkins
 """
 
 import numpy as np
-
-def test_data(states, npts):
-        """ Generate simulated trajectory of given length with a given set of states """
-        d = np.empty(npts)
-        i = 0
-        state = 0
-        while i < npts:
-                n,rate = states[state]
-                dt = np.random.poisson(n)
-                if i+dt >= npts:
-                        d.resize(i)
-                        return d
-                d[i:i+dt] = np.random.poisson(rate, dt)
-                i += dt
+from numpy import log
 
 def compute_likelihood(times):
         """ Compute the log-likelihood of a changepoint occurring at each timestep.
@@ -30,11 +17,11 @@ def compute_likelihood(times):
             Each element of the returned array L[k] corresponds to the log-likelihood of
             a changepoint occurring at photon times[k].
         """
-        N = len(data)
+        N = len(times)
         t = times - times[0]
         T = t[-1]
 
-        k = np.indices(photons.shape)
+        k = np.indices(times.shape)
         Vk = t[k] / T
         L = 2*k*log(k/Vk) + 2*(N-k)*log((N-k)/(1-Vk)) - 2*N*log(N)
         return L
@@ -42,7 +29,7 @@ def compute_likelihood(times):
 def find_tau(times, alpha):
         L = compute_likelihood(times)
         N = len(times)
-        np.count(L <= tau) / (N-1) - 
+        np.count(L <= tau) / (N-1) 
         
 def find_change_point(times, alpha=0.05):
         for k in range(len(times)):
@@ -57,10 +44,7 @@ def find_change_points(times, alpha=0.05):
         tau = compute_tau(1-beta)
         C = (Z-L) <= tau
 
-
-if __name__ == '__main__':
-        #d = test_data(states, 1e5)
-
+def test_data():
         times = []
         t = 0
         for i in range(1e5):
@@ -71,8 +55,10 @@ if __name__ == '__main__':
                 dt = np.random.exponential(2)
                 t += dt
                 times.append(t)
-        times = np.array(times)
+        return np.array(times)
 
+if __name__ == '__main__':
+        times = test_data()
         L = compute_likelihood(times)[0][1:-1]
 
         import matplotlib.pyplot as pl
