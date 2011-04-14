01729fe74267900aa0606d2b95667b4af2691e7e
Ben Gamari <bgamari.foss@gmail.com>
2011-04-14 15:51:31 -0400

    bin_photons: Produce bin start times in cython code

--- _bin_photons.pyx
+++ _bin_photons.pyx
@@ -2,11 +2,19 @@ import sys
 import numpy as np
 cimport numpy as np
 
+ctypedef unsigned short uint16_t
 ctypedef unsigned long long uint64_t
 
-def bin_photons(np.ndarray[np.uint64_t, ndim=1] times, int bin_width):
+cdef packed struct Bin:
+        uint64_t start_t
+        uint16_t count
+bin_dtype = np.dtype([
+        ('start_t', np.uint64),
+        ('count', np.uint16)])
+
+def bin_photons(np.ndarray[np.uint64_t] times, int bin_width, bool include_zeros=True):
         cdef unsigned int chunk_sz = 10000
-        cdef np.ndarray[np.uint16_t, ndim=1] chunk = np.empty(chunk_sz, dtype=np.uint16)
+        cdef np.ndarray[Bin] chunk = np.empty(chunk_sz, dtype=bin_dtype)
         chunks = []
 
         cdef Py_ssize_t i, j
@@ -18,21 +26,24 @@ def bin_photons(np.ndarray[np.uint64_t, ndim=1] times, int bin_width):
         for i in range(times.shape[0]):
                 if times[i] >= bin_start + bin_width:
                         new_start = (times[i] / bin_width) * bin_width
-                        chunk[bin] = bin_count
+                        chunk[bin].start_t = bin_start
+                        chunk[bin].count = bin_count
                         bin += 1
                         if bin == chunk_sz:
                                 chunks.append(chunk)
-                                chunk = np.empty(chunk_sz, dtype=np.uint16)
+                                chunk = np.empty(chunk_sz, dtype=bin_dtype)
                                 bin = 0
 
                         # Account for zero bins
-                        for j in range(bin_start+bin_width, new_start, bin_width):
-                                chunk[bin] = 0
-                                bin += 1
-                                if bin == chunk_sz:
-                                        chunks.append(chunk)
-                                        chunk = np.empty(chunk_sz, dtype=np.uint16)
-                                        bin = 0
+                        if include_zeros:
+                                for j in range(bin_start+bin_width, new_start, bin_width):
+                                        chunk[bin].start_t = j
+                                        chunk[bin].count = 0
+                                        bin += 1
+                                        if bin == chunk_sz:
+                                                chunks.append(chunk)
+                                                chunk = np.empty(chunk_sz, dtype=bin_dtype)
+                                                bin = 0
 
                         bin_count = 0
                         bin_start = new_start
