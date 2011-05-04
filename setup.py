6f83671cbceb0ee262e409edf112bbae5a83b5cc
Ben Gamari <bgamari.foss@gmail.com>
2011-05-04 11:01:24 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+ext_modules = [
+        Extension('_bin_photons', ['_bin_photons.pyx'])
+]
+
+setup(name = 'photon-tools',
+      author = 'Ben Gamari',
+      author_email = 'bgamari@physics.umass.edu',
+      url = 'http://goldnerlab.physics.umass.edu/',
+      description = 'Tools for manipulating photon data from single-molecule experiments',
+      version = '1.0',
+      package_dir = {'photon_tools': '.'},
+      packages = ['photon_tools'],
+      scripts = ['bin_photons', 'fcs-fit'],
+      license = 'GPLv3',
+      cmdclass = {'build_ext': build_ext},
+      ext_modules = ext_modules,
+)
+
