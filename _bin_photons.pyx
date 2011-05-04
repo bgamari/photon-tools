500e663d61e696f25d52448f90a5a71a32769f6b
Ben Gamari <bgamari.foss@gmail.com>
2011-05-04 11:07:51 -0400

    bin_photons: Add docstring

--- _bin_photons.pyx
+++ _bin_photons.pyx
@@ -14,6 +14,17 @@ bin_dtype = np.dtype([
         ('count', np.uint16)])
 
 def bin_photons(np.ndarray[np.uint64_t] times, int bin_width, bool include_zeros=True):
+        """ bin_photons(times, bin_width, include_zeros=True)
+
+        Bin the given array of photon times in bins of bin_width. The resulting
+        bins are returned in a record array containing the fields,
+          - start_t (u8)
+          - count (u2)
+        
+        By default, the resulting array includes all bins, even those
+        containing no photons. Setting include_zeros to False will result in
+        only bins which contain at least one photon.
+        """
         cdef unsigned int chunk_sz = 10000
         cdef np.ndarray[Bin] chunk = np.empty(chunk_sz, dtype=bin_dtype)
         chunks = []
