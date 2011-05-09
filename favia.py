9db5c03a55955eb20639a7d892e9cd5e5ea51137
Ben Gamari <bgamari.foss@gmail.com>
2011-05-09 14:32:55 -0400

    favia: take jiffy instead of clockrate

--- favia.py
+++ favia.py
@@ -6,14 +6,16 @@ dtype = np.dtype([('lag', 'f8'), ('loglag', 'f8'),
                   ('dot', 'f8'), ('dotnormed', 'f8'),
                   ('bar', 'f8')])
 
-def acorr(x, clockrate=160e6, short_grain=1e-6, long_lag=1):
-        return corr(x, x, clockrate, short_grain, long_lag)
+def acorr(x, jiffy=1./128e6, short_grain=1e-6, long_lag=1):
+        return corr(x, x, jiffy, short_grain, long_lag)
 
-def corr(x, y, clockrate=160e6, short_grain=1e-6, long_lag=1):
+def corr(x, y, jiffy=1./128e6, short_grain=1e-6, long_lag=1):
+        """ Compute the correlation function of the datasets x and y. Jiffy,
+        short_grain, and long_lag are given in seconds """
         fx = NamedTemporaryFile()
         fy = NamedTemporaryFile()
-        np.tofile(fx, x)
-        np.tofile(fy, y)
+        x.tofile(fx)
+        y.tofile(fy)
         args = ['favia',
                 '--xfile=%s ' % fx.name, '--yfile=%s' % fy.name,
                 '--jiffy=%f' % 1./clockrate,
