d527caaadf2d7930546d5c2bacfa5d66bbd2b966
Lori Goldner <lori@blinky.(none)>
2011-06-10 18:03:54 -0400

    favia: Add verbose

--- favia.py
+++ favia.py
@@ -1,3 +1,4 @@
+import sys
 from tempfile import NamedTemporaryFile
 import subprocess
 import numpy as np
@@ -6,10 +7,10 @@ dtype = np.dtype([('lag', 'f8'), ('loglag', 'f8'),
                   ('dot', 'f8'), ('dotnormed', 'f8'),
                   ('bar', 'f8')])
 
-def acorr(x, jiffy=1./128e6, short_grain=1e-6, long_lag=1):
-        return corr(x, x, jiffy, short_grain, long_lag)
+def acorr(x, jiffy=1./128e6, short_grain=1e-6, long_lag=1, verbose=False):
+        return corr(x, x, jiffy, short_grain, long_lag, verbose)
 
-def corr(x, y, jiffy=1./128e6, short_grain=1e-6, long_lag=1):
+def corr(x, y, jiffy=1./128e6, short_grain=1e-6, long_lag=1, verbose=False):
         """ Compute the correlation function of the datasets x and y. Jiffy,
         short_grain, and long_lag are given in seconds """
         fo = NamedTemporaryFile()
@@ -23,7 +24,8 @@ def corr(x, y, jiffy=1./128e6, short_grain=1e-6, long_lag=1):
                 '--jiffy=%e' % jiffy,
                 '--long_lag=%e' % long_lag,
                 '--short_grain=%e' % short_grain]
-        p = subprocess.Popen(args, stdout=fo, stderr=subprocess.PIPE)
+        stderr = sys.stderr if verbose else subprocess.PIPE
+        p = subprocess.Popen(args, stdout=fo, stderr=stderr)
         if p.wait() != 0:
                 print p.stderr.read()
                 raise RuntimeError('Favia threw error')
