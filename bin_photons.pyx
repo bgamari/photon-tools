63e125fdfbef31971628be3278cbab45ca85c453
Richard Buckman <rich@rich-desktop.(none)>
2011-06-13 14:32:18 -0400

    bin_photons: Add start_t and end_t parameters

--- bin_photons.pyx
+++ bin_photons.pyx
@@ -2,7 +2,7 @@ from cpython cimport bool
 from timetag_types cimport *
 from timetag_types import *
 
-def bin_photons(np.ndarray[np.uint64_t] times, int bin_width, bool include_zeros=True):
+def bin_photons(np.ndarray[np.uint64_t] times, int bin_width, uint64_t start_t=-1, uint64_t end_t=-1, bool include_zeros=True):
         """ bin_photons(times, bin_width, include_zeros=True)
 
         Bin the given array of photon times in bins of bin_width. The resulting
@@ -20,7 +20,8 @@ def bin_photons(np.ndarray[np.uint64_t] times, int bin_width, bool include_zeros
 
         cdef Py_ssize_t i, j
         cdef uint64_t new_start
-        cdef uint64_t bin_start = (times[0] / bin_width) * bin_width
+        cdef uint64_t bin_start = times[0] if start_t == -1 else start_t
+        bin_start = (bin_start / bin_width) * bin_width # Make sure we start on bin boundary
         cdef short bin_count = 0
         cdef unsigned int bin = 0
 
@@ -51,6 +52,17 @@ def bin_photons(np.ndarray[np.uint64_t] times, int bin_width, bool include_zeros
 
                 bin_count += 1
 
+        if include_zeros and end_t != -1:
+                for j in range(bin_start+bin_width, end_t, bin_width):
+                        chunk[bin].start_t = j
+                        chunk[bin].count = 0
+                        bin += 1
+                        if bin == chunk_sz:
+                                chunks.append(chunk)
+                                chunk = np.empty(chunk_sz, dtype=bin_dtype)
+                                bin = 0
+
         chunks.append(chunk[:bin])
         return np.hstack(chunks)
+
 
