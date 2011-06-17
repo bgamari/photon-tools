bcc6df19c2f186a5cdf83c997f457ee383bb38f9
Richard Buckman <rich@rich-desktop.(none)>
2011-06-17 15:18:10 -0400

    float bug in plot-fret, some changes to alex.py

--- alex.py
+++ alex.py
@@ -11,14 +11,15 @@ from matplotlib import pyplot as pl
 from numpy import mean, std, amin, amax, logical_and
 
 def shifted_deltas(deltas, state, off):
-        """ Add an offset to the start times of delta records with the given state """
-        ret = np.copy(deltas)
-        taken = deltas['state'] == state
-        ret['start_t'][taken] += off*delta_clock
-        return ret
+	""" Add an offset to the start times of delta records with the given state """
+	ret = np.copy(deltas)
+	if (off != 0):
+		taken = deltas['state'] == state
+		ret['start_t'][taken] += off*delta_clock
+	return ret
 
 AlexBins = namedtuple('AlexBins', 'Dem_Dexc Dem_Aexc Aem_Dexc Aem_Aexc')
-def get_alex_bins(strobe_D, strobe_A, delta_D, delta_A):
+def get_alex_bins(strobe_D, strobe_A, delta_D, delta_A, bin_width, strobe_clock, start_exc_offset = 0):
 	Dem_Dexc = filter_by_spans(strobe_D, shifted_deltas(delta_D, True, start_exc_offset))
 	Dem_Aexc = filter_by_spans(strobe_D, shifted_deltas(delta_A, True, start_exc_offset))
 	Aem_Dexc = filter_by_spans(strobe_A, shifted_deltas(delta_D, True, start_exc_offset))
@@ -57,7 +58,7 @@ if __name__ == '__main__':
 	delta_D = get_delta_events(f, 0, skip_wraps=skip_wraps)
 	delta_A = get_delta_events(f, 1, skip_wraps=skip_wraps)
 
-	bins = get_alex_bins(strobe_D, strobe_A, delta_D, delta_A)
+	bins = get_alex_bins(strobe_D, strobe_A, delta_D, delta_A, bin_width, strobe_clock, start_exc_offset)
 	F_Dem_Dexc = bins.Dem_Dexc['count']
 	F_Dem_Aexc = bins.Dem_Aexc['count']
 	F_Aem_Dexc = bins.Aem_Dexc['count']
@@ -83,6 +84,26 @@ if __name__ == '__main__':
 	#S_raw = 1. * (F_Dem_Dexc + F_Aem_Dexc) / (F_Dem_Dexc + F_Aem_Dexc + F_Aem_Aexc)
 	#S_raw = S_raw[np.logical_not(np.isnan(S_raw))]
 
+
+	def alex_e_raw(Dem_Dexc, Dem_Aexc, Aem_Dexc, Aem_Aexc):
+		return 1.0 * Aem_Dexc / (Aem_Dexc + Dem_Dexc)
+
+	def alex_s_raw(Dem_Dexc, Dem_Aexc, Aem_Dexc, Aem_Aexc):
+		return 1.0 * (Dem_Dexc + Aem_Dexc) / (Dem_Dexc + Aem_Dexc + Aem_Aexc)
+
+	def plot_alex_raw(ax, Dem_Dexc, Dem_Aexc, Aem_Dexc, Aem_Aexc, thresh):
+		ctot = Dem_Dexc + Dem_Aexc + Aem_Dexc + Aem_Aexc
+		t = thresh * mean(ctot)
+		take = ctot > t
+		tdd, tda, tad, taa = Dem_Dexc[take], Dem_Aexc[take], Aem_Dexc[take], Aem_Aexc[take]
+		ax.locator_params(nbins=4)
+		if len(tdd) > 0:
+			e_raw = alex_e_raw(tdd, tda, tad, taa)
+			s_raw = alex_s_raw(tdd, tda, tad, taa)
+			ax.plot(e_raw[:10000], s_raw[:10000], 'b+')
+		ax.text(0.1, 0.75, '$%1.2f bkgrd \/(I > %1.2f \/\mathrm{ ppb})$' % (thresh, t), transform=ax.transAxes)
+		
+
 	def fret_eff(don_bins, acc_bins):
 		return 1. * acc_bins / (don_bins + acc_bins)
 
@@ -96,12 +117,17 @@ if __name__ == '__main__':
 			ax.hist(fret_eff(td, ta), bins=20, histtype='step', range=(0,1))
 			ax.set_xlabel('FRET Efficiency')
 			ax.set_ylabel('Events')
-		ax.text(0.1, 0.75, '$%1.2f bkgrd \/(I > %1.2f \/\mathrm{ppb})$' % (thresh, t), transform=ax.transAxes)
+		ax.text(0.1, 0.75, '$%1.2f bkgrd \/(I > %1.2f \/\mathrm{ ppb})$' % (thresh, t), transform=ax.transAxes)
 
-	plot_fret_eff_hist(pl.subplot(221), F_Dem_Dexc, F_Aem_Dexc, 0.0)
-	plot_fret_eff_hist(pl.subplot(222), F_Dem_Dexc, F_Aem_Dexc, 1.0)
-	plot_fret_eff_hist(pl.subplot(223), F_Dem_Dexc, F_Aem_Dexc, 1.5)
-	plot_fret_eff_hist(pl.subplot(224), F_Dem_Dexc, F_Aem_Dexc, 2.0)
+	plot_fret_eff_hist(pl.subplot(241), F_Dem_Dexc, F_Aem_Dexc, 0.0)
+	plot_fret_eff_hist(pl.subplot(242), F_Dem_Dexc, F_Aem_Dexc, 1.0)
+	plot_fret_eff_hist(pl.subplot(243), F_Dem_Dexc, F_Aem_Dexc, 1.5)
+	plot_fret_eff_hist(pl.subplot(244), F_Dem_Dexc, F_Aem_Dexc, 2.0)
+	plot_alex_raw(pl.subplot(245), F_Dem_Dexc, F_Dem_Aexc, F_Aem_Dexc, F_Aem_Aexc, 0.0)
+	plot_alex_raw(pl.subplot(246), F_Dem_Dexc, F_Dem_Aexc, F_Aem_Dexc, F_Aem_Aexc, 1.0)
+	plot_alex_raw(pl.subplot(247), F_Dem_Dexc, F_Dem_Aexc, F_Aem_Dexc, F_Aem_Aexc, 1.5)
+	plot_alex_raw(pl.subplot(248), F_Dem_Dexc, F_Dem_Aexc, F_Aem_Dexc, F_Aem_Aexc, 2.0)
+
 
 	#pl.plot(E_raw_PR[:10000], S_raw[:10000], 'bo')
 	if args.output is None:
