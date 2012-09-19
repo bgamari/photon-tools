acf9406de6535dd6f09e43323fee4b9556cd7c91
Sheema Rahmanseresht <sheema@rostam.(none)>
2012-09-19 15:09:54 -0400

    pt2: Don't throw *runtime_error

--- pt2.cpp
+++ pt2.cpp
@@ -31,7 +31,7 @@ pt2_record pt2_file::read_record() {
 
 	records_read++;
 	if (records_read > tttr_hdr.n_records)
-		throw new std::runtime_error("Read too many records");
+		throw std::runtime_error("Read too many records");
 
 	is.read((char*) &rec, 4);
 	r.time = 0x0fffffff & rec;
@@ -63,13 +63,13 @@ std::vector<pt2_record> pt2_file::read_all_records() {
 void pt2_file::read_headers() {
 	is.read((char*) &text_hdr, sizeof(pt2_text_hdr));
 	if (strcmp(text_hdr.ident, "PicoHarp 300"))
-		throw new std::runtime_error("File identifier not found");
+		throw std::runtime_error("File identifier not found");
 	if (strncmp(text_hdr.format_version, "2.0", 3))
-		throw new std::runtime_error("Unsupported file format version");
+		throw std::runtime_error("Unsupported file format version");
 
 	is.read((char*) &binary_hdr, sizeof(pt2_binary_hdr));
 	if (binary_hdr.meas_mode != PT2_MEASMODE_T2)
-		throw new std::runtime_error("Unsupported measurement mode");
+		throw std::runtime_error("Unsupported measurement mode");
 
 	is.read((char*) &board_hdr, sizeof(pt2_board_hdr));
 	is.read((char*) &tttr_hdr, sizeof(pt2_tttr_hdr));
