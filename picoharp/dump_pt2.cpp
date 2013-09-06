53bd2cc331a7cf8d532716658ca45ed898f0e51d
Ben Gamari <bgamari.foss@gmail.com>
2013-09-06 18:35:54 -0400

    dump_pt2: Dump jiffy

--- picoharp/dump_pt2.cpp
+++ picoharp/dump_pt2.cpp
@@ -27,6 +27,7 @@ int main(int argc, char** argv) {
 	std::ifstream is(argv[1]);
 	pt2_file pt2(is);
 
+	printf("# jiffy = %f\n", pt2.board_hdr.resolution);
 	for (unsigned int i=0; i < pt2.tttr_hdr.n_records; i++) {
 		pt2_record rec = pt2.read_record();
 		if (rec.special) {
