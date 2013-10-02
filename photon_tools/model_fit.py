ecdff5205b42214281861bffb79737d9bcbd7418
Ben Gamari <bgamari.foss@gmail.com>
2013-10-02 15:19:53 -0400

    model_fit: Enable residual hline in plotting

--- photon_tools/model_fit.py
+++ photon_tools/model_fit.py
@@ -172,8 +172,8 @@ def plot_model(fig, ax, params, model, curve_names, npts=1e3):
                 ax2 = divider.append_axes('top', size=1.2, pad=0.1, sharex=ax)
                 d = params.curves[i]
                 ax2.semilogx(d['lag'], model(cparams, d['lag']) - d['G'], '+')
-                # This appears to be broken at the moment due to Issue #1246
-                #ax2.axhline(y=1, color='k', alpha=0.5)
+                # This was broken in matplotlib <1.2 due to Issue #1246
+                ax2.axhline(y=0, color='k', alpha=0.5)
                 for t in ax2.get_xticklabels():
                         t.set_visible(False)
 
