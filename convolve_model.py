2f3f6411c567a729294c81983d66e8f397baa035
Ben Gamari <bgamari.foss@gmail.com>
2014-12-04 09:47:35 -0500

    convolve_model: Properly invoke submodel

--- convolve_model.py
+++ convolve_model.py
@@ -4,9 +4,8 @@ from scipy.signal import fftconvolve
 class ConvolveModel(Model):
     def __init__(self, response, model, *args, **kwargs):
         def convolve(x, **kwargs):
-            #model_args = {k: v for k,v in kwargs.items() if k in model.param_names}
-            model_args = model.make_funcargs(kwargs=kwargs)
-            m = model.func(x, **model_args)
+            kwargs['x'] = x
+            m = model.eval(**kwargs)
             a = fftconvolve(response, m, 'valid')
             a = a[:x.shape[0]]
             return a
