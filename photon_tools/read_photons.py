12b54efc34ed54aa999bfe8c97500931cac84f38
Ben Gamari <bgamari.foss@gmail.com>
2014-12-05 11:13:42 -0500

    Refactor read_photons

--- photon_tools/read_photons.py
+++ photon_tools/read_photons.py
@@ -1,14 +1,6 @@
 import numpy as np
 from photon_tools import timetag_parse, pt2_parse, metadata
 
-def determine_filetype(fname):
-    if fname.endswith('pt2'):       return 'pt2'
-    elif fname.endswith('pt3'):     return 'pt2'
-    elif fname.endswith('timetag'): return 'timetag'
-    elif fname.endswith('times'):   return 'raw'
-    elif fname.endswith('timech'):     return 'timech'
-    raise RuntimeError("Unrecognized file type")
-    
 def verify_monotonic(times):
     """ Verify that timestamps are monotonically increasing """
     if len(times) == 0: return
@@ -30,45 +22,90 @@ def verify_continuity(times, gap_factor=1000):
             print('    starting at %10d, ending at %10d, lasting %10d' %
                   (times[s], times[s+1], times[s+1] - times[s]))
 
-class TimestampFile(object):
-    """ A portable interface for reading photon timestamp files """
-    def __init__(self, fname, channel, ftype=None):
-        """ TimestampFile(filename, channel)
+class TimestampReader(object):
+    extensions = []
+    def __init__(self, fname, channel):
+        raise Unimplemented
 
-            Channel number is zero-based """
+class Pt2File(TimestampReader):
+    """ Read Picoquant PT2 timestamp files """
+    extensions = ['pt2']
+    def __init__(self, fname, channel):
+        self.jiffy = 4e-12 # FIXME
+        self.data = pt2_parse.read_pt2(fname, channel)
+
+class TimetagFile(TimestampReader):
+    """ Read Goldner FPGA timetagger files """
+    extensions = ['timetag']
+    def __init__(self, fname, channel):
+        if channel not in range(4):
+            raise RuntimeError(".timetag files only have channels 0..3")
+        self.metadata = metadata.get_metadata(fname)
+        if self.metadata is not None:
+            self.jiffy = 1. / self.metadata['clockrate']
+        open(fname) # Ensure a reasonable error is generated
+        self.data = timetag_parse.get_strobe_events(fname, 1<<channel)['t']
+
+class RawFile(TimestampReader):
+    """ Read raw unsigned 64-bit timestamps """
+    extensions = ['raw']
+    def __init__(self, fname, channel):
+        if channel != 0:
+            raise RuntimeError("Raw timetag files have only channel 0")
+        self.data = np.fromfile(fname, dtype='u8')
+
+class RawChFile(TimestampReader):
+    """ Read raw unsigned 64-bit timestamps, followed by 8-bit channel number """
+    extensions = 'timech'
+    def __init__(self, fname, channel):
+        if channel > 255:
+            raise RuntimeError("Raw timetag files have only 256 channels")
+        d = np.fromfile(fname, dtype='u8,u1', names='time,chan')
+        self.data = d[d['chan'] == channel]['time']
+
+readers = [
+    Pt2Reader,
+    TimetagReader,
+    RawReader,
+    RawTimeChReader,
+]
+
+def reader_extensions():
+    """
+    Construct a map from supported file extensions to their
+    associated reader.
+    """
+    extensions = {}
+    for reader in readers:
+        for ext in reader.extensions:
+            extensions[ext] = reader
+    return extensions
+
+def find_reader(fname):
+    exts = reader_extensions()
+    root,ext = os.path.splitext(fname)
+    return exts.get(ext)
+
+class TimestampFile(object):
+    """
+    A format-agnostic interface for reading photon timestamp files.
+    """
+
+    @staticmethod
+    def open(fname, channel, reader=None):
+        """
+        TimestampFile(filename, channel)
+
+        Channel number is zero-based.
+        """
         self.jiffy = None
         self.metadata = None
 
-        if ftype is None:
-            ftype = determine_filetype(fname)
-
-        if ftype == 'pt2':
-            self.jiffy = 4e-12
-            self.data = pt2_parse.read_pt2(fname, channel)
-
-        elif ftype == 'timetag':
-            if channel not in range(4):
-                raise RuntimeError(".timetag files only have channels 0..3")
-            self.metadata = metadata.get_metadata(fname)
-            if self.metadata is not None:
-                self.jiffy = 1. / self.metadata['clockrate']
-            open(fname) # Ensure a reasonable error is generated
-            self.data = timetag_parse.get_strobe_events(fname, 1<<channel)['t']
-
-        elif ftype == 'raw':
-            if channel != 0:
-                raise RuntimeError("Raw timetag files have only channel 0")
-            self.data = np.fromfile(fname, dtype='u8')
-
-        elif ftype == 'timech':
-            if channel > 255:
-                raise RuntimeError("Raw timetag files have only 256 channels")
-            d = np.fromfile(fname, dtype='u8,u1', names='time,chan')
-	    self.data = d[d['chan'] == channel]['time']
-
-        else:
+        if reader is None:
+            reader = determine_filetype(fname)
+        if reader is None:
             raise RuntimeError("Unknown file type")
 
+        reader.read(fname, channel)
         verify_monotonic(self.data)
         verify_continuity(self.data)
-            
