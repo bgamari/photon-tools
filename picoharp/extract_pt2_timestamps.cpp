36ad54d7b3111280d811bb647762229ae2b8936d
Ben Gamari <ben@smart-cactus.org>
2015-01-31 18:08:10 -0500

    extract_pt2_timestamps: Detect measurement mode

--- picoharp/extract_pt2_timestamps.cpp
+++ picoharp/extract_pt2_timestamps.cpp
@@ -26,36 +26,55 @@
 #include "pt3.h"
 
 // channel=0xf dumps all channels
-void dump_pt2(std::istream& is, std::ostream& os, uint8_t channel=0xf)
+void dump_pt2(pt2_file& pt2, std::ostream& os, uint8_t channel=0xf)
 {
-        pt2_file pt2(is);
-	unsigned int n_rec = pt2.tttr_hdr.n_records;
+        unsigned int n_rec = pt2.tttr_hdr.n_records;
 
-	for (int i=0; i < n_rec; i++) {
-		pt2_record rec = pt2.read_record();
-		if (!rec.special
+        for (int i=0; i < n_rec; i++) {
+                pt2_record rec = pt2.read_record();
+                if (!rec.special
                     && (channel == 0xf || channel == rec.channel)) {
-			uint64_t time = rec.time;
-			int count = 1;
-			os.write((char*) &time, sizeof(uint64_t));
-		}
-	}
+                        uint64_t time = rec.time;
+                        int count = 1;
+                        os.write((char*) &time, sizeof(uint64_t));
+                }
+        }
 }
 
-void dump_pt3(std::istream& is, std::ostream& os, uint8_t channel=0xf)
+void dump_pt3(pt3_file& pt3, std::ostream& os, uint8_t channel=0xf)
 {
-        pt3_file pt3(is);
-	unsigned int n_rec = pt3.tttr_hdr.n_records;
+        unsigned int n_rec = pt3.tttr_hdr.n_records;
 
-	for (int i=0; i < n_rec; i++) {
-		pt3_record rec = pt3.read_record();
-		if (!rec.is_special
+        for (int i=0; i < n_rec; i++) {
+                pt3_record rec = pt3.read_record();
+                if (!rec.is_special
                     && (channel == 0xf || channel == rec.channel)) {
-			uint64_t time = rec.normal.time;
-			int count = 1;
-			os.write((char*) &time, sizeof(uint64_t));
-		}
-	}
+                        uint64_t time = rec.normal.time;
+                        int count = 1;
+                        os.write((char*) &time, sizeof(uint64_t));
+                }
+        }
+}
+
+void dump(std::istream& is, std::ostream& os, uint8_t channel=0xf)
+{
+        picoharp_file pt(is);
+        switch (pt.binary_hdr.meas_mode) {
+        case PT2_MEASMODE_T3:
+        {
+                pt3_file pt3(pt);
+                dump_pt3(pt3, os, channel);
+                break;
+        }
+        case PT2_MEASMODE_T2:
+        {
+                pt2_file pt2(pt);
+                dump_pt2(pt2, os, channel);
+                break;
+        }
+        default:
+                throw std::runtime_error("Unsupported measurement mode");
+        }
 }
 
 bool is_suffix(std::string str, std::string suffix)
@@ -77,10 +96,10 @@ int main(int argc, char** argv)
                 if (strncmp(argv[1], "-c", 2) == 0) {
                         if (argc < 3) return -1;
                         chan = atoi(argv[2]);
-                        dump_pt2(std::cin, std::cout, chan);
+                        dump(std::cin, std::cout, chan);
                         return 0;
                 }
-                
+
                 if (argc > 2) {
                         chan = atoi(argv[2]);
                         out_name = (boost::format("%s.ch%d.times") % name % chan).str();
@@ -90,14 +109,7 @@ int main(int argc, char** argv)
                 }
 
                 std::ofstream os(out_name.c_str());
-                if (is_suffix(name, "pt2")) {
-                        dump_pt2(is, os, chan);
-                } else if (is_suffix(name, "pt3")) {
-                        dump_pt3(is, os, chan);
-                } else {
-                        std::cerr << "Unrecognized file format\n";
-                        return 1;
-                }
+                dump(is, os, chan);
 
                 std::ofstream pos((out_name + ".meta").c_str());
                 pos << (boost::format("{\n"
@@ -106,9 +118,8 @@ int main(int argc, char** argv)
                                       "}") % (1.0/PT2_TIME_UNIT)
                                            % "todo");
         } else {
-                dump_pt2(std::cin, std::cout);
+                dump(std::cin, std::cout);
         }
 
-	return 0;
+        return 0;
 }
-
