e9a7d81b4464c910b6667de71857ca2cf9109974
Ben Gamari <ben@smart-cactus.org>
2015-04-17 15:45:19 -0400

    More comments

--- photon_tools/io/timetag_parse.pyx
+++ photon_tools/io/timetag_parse.pyx
@@ -25,6 +25,21 @@ ELSE:
         print 'Invalid ENDIANESS'
 
 def get_strobe_events(f, channel_mask, skip_wraps=1):
+        """
+        Returns a list of delta channel events. The event is defined by a start
+        time past which the state is the given value.
+
+        :type f: :class:`file` or :class:`str` (file name)
+        :param f: timetag file
+        :type strobe_mask: int
+        :param strobe_mask: Logical bitmask of strobe channels of interest
+            (e.g. ``1`` selects channel 0, ``5`` selects channels 0 and 2)
+        :type skip_wraps: int
+        :param skip_wraps: How many timestamp wraparounds to skip at the beginning of the data set.
+            This was introduced to work around hardware limitations where
+            "stale" records from the end of the previous dataset would appear at the beginning
+            of the data stream.
+        """
         cdef char* fname
         if isinstance(f, str):
                 fname = f
@@ -86,6 +101,16 @@ def get_delta_events(f, channel, skip_wraps=1):
         """
         Returns a list of delta channel events. The event is defined by a start
         time past which the state is the given value.
+
+        :type f: :class:`file` or :class:`str` (file name)
+        :param f: timetag file
+        :type strobe_mask: int
+        :param strobe_mask: Logical bitmask of strobe channels of interest
+        :type skip_wraps: int
+        :param skip_wraps: How many timestamp wraparounds to skip at the beginning of the data set.
+            This was introduced to work around hardware limitations where
+            "stale" records from the end of the previous dataset would appear at the beginning
+            of the data stream.
         """
         cdef char* fname
         if isinstance(f, str):
@@ -177,6 +202,11 @@ def get_filtered_strobe_events(f, strobe_mask, delta_channel, skip_wraps=-1,
         :param strobe_mask: Logical bitmask of strobe channels of interest
         :type delta_channel: int
         :param delta_channel: Delta channel number of interest (first channel is ``0``)
+        :type skip_wraps: int
+        :param skip_wraps: How many timestamp wraparounds to skip at the beginning of the data set.
+            This was introduced to work around hardware limitations where
+            "stale" records from the end of the previous dataset would appear at the beginning
+            of the data stream.
         :type on_offset: int
         :param on_offset: Dead time after initial delta turn-on. This allows one
             to drop photon arrivals occuring during the transient turn-on interval
