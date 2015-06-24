88169d7ec06cf7ea640508f37da173a172235761
Ben Gamari <ben@smart-cactus.org>
2015-06-24 17:19:41 +0200

    anisotropy: Python3 compat

--- photon_tools/anisotropy.py
+++ photon_tools/anisotropy.py
@@ -355,9 +355,9 @@ def plot(fig, corrs, jiffy_ps, result, sep_resid=False, opacity=0.4):
         if sep_resid:
             axes.locator_params(axis='y', nbins=2)
 
-    residuals.values()[len(corrs)//2].set_ylabel('residual')
-    residuals.values()[len(corrs)-1].set_xlabel('time (ns)')
-    pl.setp(residuals.values()[len(corrs)-1].get_xticklabels(), visible=True)
+    list(residuals.values())[len(corrs)//2].set_ylabel('residual')
+    list(residuals.values())[len(corrs)-1].set_xlabel('time (ns)')
+    list(pl.setp(residuals.values())[len(corrs)-1].get_xticklabels(), visible=True)
     plots.set_yscale('log')
     pl.setp(plots.get_xticklabels(), visible=False)
 
