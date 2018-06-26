a61b4430ee8eeba800d3cbce17917d9ecb089d67
Ben Gamari <ben@smart-cactus.org>
2018-06-25 20:21:12 -0400

    nix: Capture squmfit dependency

--- default.nix
+++ default.nix
@@ -1,6 +1,18 @@
-{ python3Packages }:
+{ fetchFromGitHub, python3Packages }:
 
-python3Packages.buildPythonPackage {
+let
+  squmfit = python3Packages.buildPythonPackage {
+    pname = "squmfit";
+    version = "0.1";
+    src = fetchFromGitHub {
+      owner = "bgamari";
+      repo = "squmfit";
+      rev = "e1d14055eb8f840d837b7e10f11a8ddfdac208c7";
+      sha256 = "0486rr4y8nik4mrpwlh2xswqr2gpiqwp7lpfj7jqjh68ycdlfk9h";
+    };
+    propagatedBuildInputs = with python3Packages; [numpy scipy matplotlib];
+  };
+in python3Packages.buildPythonPackage {
   pname = "photon-tools";
   version = "0.1";
   src = ./.;
